e946dc12c87ce5932436201e22ceb2c391c23ed4
Mike Coon <mac718@gmail.com>
2022-02-06 17:48:12 -0800

    Add tests for Board

---
+++ src/components/Board.test.tsx
@@ -0,0 +1,46 @@
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Board from "./Board";
+
+describe("Board", () => {
+  beforeEach(() => {
+    render(<Board />);
+  });
+
+  it("renders 9 empty cells on load", () => {
+    const cells = document.querySelectorAll(".empty-cell");
+
+    expect(cells.length).toBe(9);
+  });
+
+  it("displays an 'X' when the user clicks an empty cell", () => {
+    const cell = document.getElementsByClassName("empty-cell")[0];
+
+    userEvent.click(cell);
+
+    const clickedCell = screen.getByText("X");
+
+    expect(clickedCell).toBeInTheDocument();
+  });
+
+  it("displays an 'O' in a different empty cell after a user marks a cell.", () => {
+    const cell = document.getElementsByClassName("empty-cell")[0];
+
+    userEvent.click(cell);
+
+    const clickedComputerCell = screen.getByText("O");
+
+    expect(clickedComputerCell).toBeInTheDocument();
+  });
+
+  it("does not trigger a computer move if and occupied cell is clicked by the user", () => {
+    const cell = document.getElementsByClassName("empty-cell")[0];
+
+    userEvent.click(cell);
+    userEvent.click(cell);
+
+    const clickedComputerCell = screen.queryAllByText("O");
+
+    expect(clickedComputerCell.length).toBe(1);
+  });
+});
